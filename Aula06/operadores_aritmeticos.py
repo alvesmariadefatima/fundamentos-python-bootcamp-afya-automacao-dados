5b94f46026b027f439c2c88858122eb7ea1ce6a8
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-05 16:35:58 -0300

    Aula sobre operadores lógicos no Python

---
+++ Aula06/operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+produto1 = 20
+produto2 = 10
+
+# Operador Soma
+print(produto1 + produto2 + 3.5)
+
+# Operador Subtração
+print(produto1 - produto2)
+
+# Operador Multiplicação
+print(produto1 * produto2)
+
+# Operador Divisão
+print(produto1 // produto2)
+
+# Operador Divisão Real
+print(produto1 / produto2)
+
+# Operador Potenciação
+print(produto1 ** produto2)
+
+x = (10 / 5) + 4
+y = (10 / 2) + (25 - 2) + (2 ** 2)
+print(x)
+print(y)
