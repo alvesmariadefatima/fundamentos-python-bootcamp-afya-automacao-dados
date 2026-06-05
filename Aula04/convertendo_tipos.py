cab775dea2d86a918096bd7aa2876e2e2f7664ff
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-05 15:46:00 -0300

    Aula sobre converter tipos de dados em python

---
+++ Aula04/convertendo_tipos.py
@@ -0,0 +1,20 @@
+# Convertendo para int
+print(int(1.0))
+
+# Convertendo uma string para int
+print(int("10"))
+
+# Convertendo uma string para float
+print(float("10.10"))
+
+# Convertendo um número int para float
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+
+# imprime tipo primitivo da variável valor
+print(type(valor))
+
+# imprime tipo primitivo da variável valor_str
+print(type(valor_str))
