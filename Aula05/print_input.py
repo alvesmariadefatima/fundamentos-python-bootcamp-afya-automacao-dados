2b9340551d6256930ff071a7c9a3fc35ac3c2fb7
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-05 16:04:19 -0300

    Aulas sobre funções de entrada e saída em Python

---
+++ Aula05/print_input.py
@@ -0,0 +1,7 @@
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
+print(nome, idade)
+print("teste", end="...\n")
+print(nome, idade, end="...\n")
+print(nome, idade, sep='#')
