ce15cfd239357c968b9b25160c628a935cc0bd53
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-05 16:46:05 -0300

    Aula sobre operadores de comparação em python

---
+++ Aula07/operadores_comparacao.py
@@ -0,0 +1,13 @@
+saldo = 200
+saque = 200
+
+# Operador de igualdade (==)
+print(saldo == saque)
+
+# Operador de diferença (!=)
+print(saldo != saque)
+
+# Operadores de comparação (<, >, <=, >=)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo <= saque)
