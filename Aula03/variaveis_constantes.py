7d9253f77bf1a5aefffc97a2e47fd40608716820
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-05 15:17:41 -0300

    Aula sobre variáveis e constantes em Python

---
+++ Aula03/variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = 'Maria de Fátima'
+
+idade = 25
+
+print(nome, idade)
+
+# Padrão Snake Case
+limite_saque_diario = 1000
+
+# Padrão Camel Case
+limiteSaqueDiario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'ES']
+
+print(BRAZILIAN_STATES)
