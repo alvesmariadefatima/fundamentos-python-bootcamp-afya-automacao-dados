ac9ea8d082bbda08fc2d1e3c71f9a02c1d84ea9b
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-08 15:03:59 -0300

    adicionar comentários para explicar melhor o código desenvolvido na aula de operadores lógicos

--- Aula09/operadores_logicos.py
+++ Aula09/operadores_logicos.py
@@ -11,15 +11,19 @@ saque = 900
 limite = 500
 conta_especial = True
 
+# exemplo de expressão com operador lógico and
 exp = saldo >= saque and saque <= limite and saldo >= saque
 
 print(exp)
 
+# exemplo de expressão com operadores lógicos and e or
 exp2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
 print(exp2)
 
+# exemplo de expressão com operadores lógicos and e or utilizando variáveis intermediárias para verificar se as contas normal e especial estão com saldo suficiente para o saque
 conta_normal_com_saldo_suficiente = (saldo >= saque and saque <= limite)
 conta_especial_com_saldo_suficiente= conta_especial and saldo >= saque
 
+# expressão utilizando as variáveis intermediárias se as contas normal e especial estão com saldo suficiente para o saque
 exp3 = conta_normal_com_saldo_suficiente or conta_especial_com_saldo_suficiente
 print(exp3)
