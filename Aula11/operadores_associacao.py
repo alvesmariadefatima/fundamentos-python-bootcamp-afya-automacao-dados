ed0dc6bb7d1ad2233dc5ff4a91b75957630734e1
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-08 15:23:34 -0300

    exemplos práticos sobre operadores de associação em python

---
+++ Aula11/operadores_associacao.py
@@ -0,0 +1,13 @@
+# IN - caso o objeto esteja presente na sequência, o operador in retorna True, caso contrário, retorna False. 
+# O operador not in é o inverso do operador in, ou seja, ele retorna True se o objeto não estiver presente na sequência e False se estiver presente.
+
+# NOT IN - caso o objeto não esteja presente na sequência, o operador not in retorna True, caso contrário, retorna False.
+# O operador in é o inverso do operador not in, ou seja, ele retorna True se o objeto estiver presente na sequência e False se não estiver presente.
+
+frutas = ["limão", "uva", "laranja"]
+curso = "Bootcamp Afya Automação de Dados"
+
+# exemplos práticos de operadores de associação in e not in
+print("laranja" in frutas)
+print("limão" not in frutas)
+print("Afya" in curso)
