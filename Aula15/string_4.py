9423bbf8884adbe9f48d1fb3f8421c54ba7f3c59
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-10 15:16:23 -0300

    exemplos de string com múltiplas linhas em python

---
+++ Aula15/string_4.py
@@ -0,0 +1,23 @@
+nome = "Fátima"
+
+# exemplo de string multilinha usando f-string
+mensagem = f"""
+    Olá, meu nome é {nome},
+Eu estou aprendendo Python no bootcamp Afya Automação de Dados & IA;
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+# exemplo de docstring de um menu interativo de caixa eletrônico
+print("""
+    ================ MENU =================
+      
+    1 - Depositar
+    2 - Sacar
+    3 - Sair
+      
+    ========================================
+      
+      Obrigado por usar nosso sistema!!!
+""")
