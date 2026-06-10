dd6b4c6ca22fa73d2556cda3681bd7732fec5a91
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-10 15:05:53 -0300

    exemplo prático de fatiamento de string em python

---
+++ Aula15/string_3.py
@@ -0,0 +1,10 @@
+nome = "Maria de Fátima Nunes Alves"
+
+# exemplos de slicing usando meu nome para fatiar a string
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:1])
+print(nome[::-1])
