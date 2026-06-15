286066fc131b0ae5401a384684346dde0a2d8707
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-15 15:23:41 -0300

    pasta voltada para o módulo estrutura de dados

---
+++ 01 - Estrutura de Dados/Aula01/00_declarando_listas.py
@@ -0,0 +1,17 @@
+# exemplo de declaração de listas
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+# exemplo de declaração de lista vazia
+frutas = []
+print(frutas)
+
+# exemplo de declaração de lista a partir de uma string, range e outra lista
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 42000, 2020, 2900, "São Paulo", True]
+print(carro)
