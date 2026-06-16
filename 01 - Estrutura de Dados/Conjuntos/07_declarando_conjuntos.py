2251fd02bcee74d574e56e479fce449ac1d2fbf3
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-16 10:53:57 -0300

    estruturação dos conteúdos referente aos módulos estrutura de dados

---
+++ 01 - Estrutura de Dados/Conjuntos/07_declarando_conjuntos.py
@@ -0,0 +1,11 @@
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)
+
+letras = set("banana")
+print(letras)
+
+carros = set({"palio", "gol", "celta", "palio"})
+print(carros)
+
+linguagens = {"Python", "java", "Python"}
+print(linguagens)
