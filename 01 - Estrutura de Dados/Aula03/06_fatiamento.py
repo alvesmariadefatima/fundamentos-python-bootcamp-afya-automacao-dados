0cedcfbfe2c0fff6a72dda3619941c581f90d330
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-16 10:31:36 -0300

    exemplo de tuplas em python e fatiamento

---
+++ 01 - Estrutura de Dados/Aula03/06_fatiamento.py
@@ -0,0 +1,9 @@
+tupla = ("p", "y", "t", "h", "o", "n")
+
+# exemplos de prints exibindo a posição dos elementos em uma tupla
+print(tupla[2:])
+print(tupla[:2])
+print(tupla[1:3])
+print(tupla[0:3:2])
+print(tupla[::])
+print(tupla[::-1])
