95d03d8550e6bcf6359dd7d3cec03808ad730722
Maria de Fátima <mnunesalves334@gmail.com>
2026-06-30 10:13:46 -0300

    exemplos práticos de herança múltipla em python

---
+++ 02 - Programação Orientada a Objetos/Herança/02_heranca_multipla.py
@@ -0,0 +1,36 @@
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+class Mamifero(Animal):
+    def __init__(self, cor_pelo=None, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
+
+class Ave(Animal):
+    def __init__(self, cor_bico=None, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+class Gato(Mamifero, Ave):
+    pass
+
+class FalarMixin:
+    def falar(self):
+        return "oi, estou falando"
+
+class Ornitorrinco(Mamifero, Ave, FalarMixin):
+    def __init__(self, cor_pelo, cor_bico, nro_patas, **kw):
+        self.cor_bico = cor_pelo
+
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+gato = Gato(nro_patas=4, cor_pelo="Preto")
+# print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+print(ornitorrinco)
+print(ornitorrinco.falar())
